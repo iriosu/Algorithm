5bf854ab7bb97d4076c9411effbbb875f6cb25a2
Ignacio Rios <ignacio.rios.u@gmail.com>
2017-07-13 11:35:42 -0700

    Update test.R

--- src/test.R
+++ src/test.R
@@ -17,29 +17,29 @@ drop_dir = 'Dropbox/PaperDEMRE/'
 setwd(paste(c(root_dir,drop_dir), collapse = ""))
 # Reading files -----------------------------------------------------------
 #carreras_requisitos
-car_req_13 = read.csv("Datos/PAUC 2013/Output/postulaciones_procesadas_reg_student_secuencial_2013.csv", sep = ";", header=TRUE)
-car_req_14 = read.csv("Datos/PAUC 2014/Output/postulaciones_procesadas_reg_student_secuencial_2014.csv", sep = ";", header=TRUE)
-car_req_15 = read.csv("Datos/PAUC 2015/Output/postulaciones_procesadas_reg_student_secuencial_2015.csv", sep = ";", header=TRUE)
-car_req_16 = read.csv("Datos/PAUC 2016/Output/postulaciones_procesadas_reg_student_secuencial_2016.csv", sep = ";", header=TRUE)
+post_reg_au_stu_13 = read.csv("Datos/PAUC 2013/Output/postulaciones_procesadas_reg_student_secuencial_2013.csv", sep = ";", header=TRUE)
+post_reg_au_stu_14 = read.csv("Datos/PAUC 2014/Output/postulaciones_procesadas_reg_student_secuencial_2014.csv", sep = ";", header=TRUE)
+post_reg_au_stu_15 = read.csv("Datos/PAUC 2015/Output/postulaciones_procesadas_reg_student_secuencial_2015.csv", sep = ";", header=TRUE)
+post_reg_au_stu_16 = read.csv("Datos/PAUC 2016/Output/postulaciones_procesadas_reg_student_secuencial_2016.csv", sep = ";", header=TRUE)
 
 #reg_sec_uni_14 = read.csv("Datos/PAUC 2014/Output/asignacion_obtenida_reg_secuencial_university_2014.csv", sep = ";", header=TRUE)
 
 codigos = c("codigo_carrera1", "codigo_carrera2", "codigo_carrera3", "codigo_carrera4", "codigo_carrera5",
     "codigo_carrera6", "codigo_carrera7", "codigo_carrera8", "codigo_carrera9", "codigo_carrera10")
 
-post_req_13 = car_req_13[codigos]
+post_req_13 = post_reg_au_stu_13[codigos]
 post_req_13$app_count = apply(post_req_13 > 0, 1, sum)
 post_req_13$year = rep(2013,nrow(post_req_13))
 
-post_req_14 = car_req_14[codigos]
+post_req_14 = post_reg_au_stu_14[codigos]
 post_req_14$app_count = apply(post_req_14 > 0, 1, sum)
 post_req_14$year = rep(2014,nrow(post_req_14))
 
-post_req_15 = car_req_15[codigos]
+post_req_15 = post_reg_au_stu_15[codigos]
 post_req_15$app_count = apply(post_req_15 > 0, 1, sum)
 post_req_15$year = rep(2015,nrow(post_req_15))
 
-post_req_16 = car_req_16[codigos]
+post_req_16 = post_reg_au_stu_16[codigos]
 post_req_16$app_count = apply(post_req_16 > 0, 1, sum)
 post_req_16$year = rep(2016,nrow(post_req_16))
 
